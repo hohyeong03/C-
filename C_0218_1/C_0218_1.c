9745c2349418ecaed0fa373c3738647af1c7778e
MKGXE <choiminhyeok77@gmail.com>
2023-02-18 10:26:45 +0900

    초기 커밋.

---
+++ C_0218_1/C_0218_1.c
@@ -0,0 +1,12 @@
+﻿// C언어 기본구조
+// printf = 입력값 출력 / f= function의 약자
+// C언어 문장 마무리는 ;(세미콜론)으로 마무리
+// \n 개행(줄바꿈)
+#include <stdio.h>
+
+int main() {
+	printf("안녕!\n");
+	printf("반갑습니다");
+
+	return 0;
+}
